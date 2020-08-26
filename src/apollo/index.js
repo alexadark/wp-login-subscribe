f9b61ee7f5e52de9117f98f154806cdb207d696a
alexaspalato <alexaspalato@gmail.com>
2020-08-26 11:08:30 +0200

    headers

--- src/apollo/index.js
+++ src/apollo/index.js
@@ -5,20 +5,32 @@ import {
   InMemoryCache,
   ApolloProvider,
   HttpLink,
+  createHttpLink,
 } from "@apollo/client"
 import fetch from "isomorphic-fetch"
 import React from "react"
+import { setContext } from "@apollo/client/link/context"
 
 import ls from "local-storage"
 
-const client = new ApolloClient({
-  cache: new InMemoryCache(),
-  link: new HttpLink({
-    uri: `${config.wordPressUrl}graphql`,
+const httpLink = createHttpLink({
+  uri: `${config.wordPressUrl}graphql`,
+})
+
+const authLink = setContext((_, { headers }) => {
+  const token = ls("authToken")
+  // return the headers to the context so httpLink can read them
+  return {
     headers: {
-      authorization: ls("authToken"),
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
     },
-  }),
+  }
+})
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
   fetch,
 })
 
